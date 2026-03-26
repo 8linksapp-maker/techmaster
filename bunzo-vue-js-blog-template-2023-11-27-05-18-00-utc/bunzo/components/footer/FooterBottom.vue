8964173318a90ca80a5d7ce83ab25b5daace4f38
8linksapp-maker <8links.app@gmail.com>
2026-03-26 17:33:16 -0300

    chore: limpeza de arquivos obsoletos do template

--- bunzo-vue-js-blog-template-2023-11-27-05-18-00-utc/bunzo/components/footer/FooterBottom.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="bg-secondary-light py-4">
-        <div class="container">
-            <div class="text-base text-center text-white">
-                <p>© 2022 Bunzo Made with <i class="icofont icofont-heart-alt text-red-600"></i> by <a href="#" target="_blank">Codecarnival</a></p>
-            </div>
-        </div>
-    </div>
-</template>
